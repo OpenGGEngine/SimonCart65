0336aa289c05768e0f5eea98129890a007ba6b22
WX <rlaterson@gmail.com>
2017-12-09 18:28:56 -0500

    SD

--- SimonCart65/src/simoncart65/components/RaceManagerComponent.java
+++ SimonCart65/src/simoncart65/components/RaceManagerComponent.java
@@ -10,6 +10,7 @@ import com.opengg.core.audio.SoundData;
 import com.opengg.core.engine.BindController;
 import com.opengg.core.engine.Resource;
 import com.opengg.core.gui.GUI;
+import com.opengg.core.gui.GUIGroup;
 import com.opengg.core.gui.GUITexture;
 import com.opengg.core.math.Vector2f;
 import com.opengg.core.model.Model;
@@ -38,6 +39,9 @@ public class RaceManagerComponent extends Component implements Actionable{
     ,new Item(Resource.getTexture("fakebox.png"),"C:\\res\\banana\\banana.bmf"),new Item(Resource.getTexture("banana.png"),"C:\\res\\banana\\banana.bmf"),new Item(Resource.getTexture("mushroom.png"),"C:\\res\\banana\\banana.bmf")};
     GUITexture itemholder;
     GUITexture item;
+    GUIGroup sidebar;
+    
+    GUITexture first,second,third,fourth;
     Sound s;
     
     UserControlComponent uc = new UserControlComponent();
@@ -49,9 +53,22 @@ public class RaceManagerComponent extends Component implements Actionable{
         s =new Sound(Resource.getSoundData("item.ogg"));
        itemholder = new GUITexture( Resource.getTexture("itemholder.png"),new Vector2f(-0.23f,0.7f), new Vector2f(0.3f,0.3f));
          item = new GUITexture(items[0].t,new Vector2f(-0.185f,0.75f), new Vector2f(0.20f,0.20f));
+         
+         first = new GUITexture(Resource.getTexture("selector.png"),new Vector2f(-0.94f,0.5f), new Vector2f(0.25f,0.25f));
+          second = new GUITexture(Resource.getTexture("secondplace.png"),new Vector2f(-0.94f,0.1f), new Vector2f(0.25f,0.25f));
+           third = new GUITexture(Resource.getTexture("thirdplace.png"),new Vector2f(-0.94f,-0.3f), new Vector2f(0.25f,0.25f));
+            fourth= new GUITexture(Resource.getTexture("fourthplace.png"),new Vector2f(-0.94f,-0.7f), new Vector2f(0.25f,0.25f));
+            
+            sidebar = new GUIGroup(new Vector2f(0,0));
+            sidebar.addItem("first", first);
+             sidebar.addItem("second", second);
+             sidebar.addItem("third", third);
+               sidebar.addItem("fourth", fourth);
         item.setLayer(-1f);
         GUI.root.addItem("itemholder", itemholder);
+        GUI.root.addItem("sidebar", sidebar);
         GUI.root.addItem("item", item);
+        
         BindController.addController(uc);
         this.attach(uc);
         
