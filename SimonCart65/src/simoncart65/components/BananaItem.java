8777a9ea7df8c0417a3ceceb83ffc229908e056b
WX <rlaterson@gmail.com>
2017-12-10 00:59:03 -0500

    SHell Shockers

---
+++ SimonCart65/src/simoncart65/components/BananaItem.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simoncart65.components;
+
+import com.opengg.core.math.Vector3f;
+import com.opengg.core.render.texture.Texture;
+
+/**
+ *
+ * @author Warren
+ */
+public class BananaItem extends Item{
+    
+    public BananaItem(Texture t, String modelpath) {
+        super(t, modelpath, new Vector3f(0.2f));
+    }
+    
+}
